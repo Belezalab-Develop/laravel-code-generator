73cfb05343acc6f545835043322b4fe5cd4e8f67
Tina Hammar <>
2019-04-17 21:17:52 +0200

    base_path error

--- src/CodeGeneratorServiceProvider.php
+++ src/CodeGeneratorServiceProvider.php
@@ -39,7 +39,7 @@ class CodeGeneratorServiceProvider extends ServiceProvider
 
         // publish frontend quasar-app
         $this->publishes([
-            $dir . 'frontend' => basepath('frontend'),
+            $dir . 'frontend' => base_path('frontend'),
         ], 'frontend');
     }
 
